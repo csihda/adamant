f14a5e5529fde323b613e3ed1187796db1945982
csihda <csihda@gmail.com>
2022-02-03 18:23:56 +0100

    finishing up dockerization process

--- src/App.js
+++ src/App.js
@@ -9,20 +9,19 @@ export default function App() {
 
   return (
     /** Use this for deploying on github-page and change the homepage in package.json to http://*.github.io/adamant */
+    /*
     <div className="the_app">
       <Switch>
         <Redirect exact from="/" to="/adamant" />
         <Route exact path="/adamant" component={AdamantMain}></Route>
       </Switch>
     </div>
-
+    */
     /** Use this for deploying with docker or on localhost and change the homepage in package.json to http://*.github.io/ */
-    /*
     <div className="the_app">
       <Switch>
         <Route exact path="/" component={AdamantMain}></Route>
       </Switch>
     </div>
-    */
   );
 };
