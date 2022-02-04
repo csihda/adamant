1665480750a332f1ae12977b0ff06af10b8444d3
csihda <csihda@gmail.com>
2022-02-04 19:23:47 +0100

    easier ghpage deployment

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import "./styles.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import AdamantMain from "./pages/AdamantMain";
 import "cors";
+import packageJson from "../package.json";
 
 export default function App() {
 
+  // check if adamant endpoint exists in the homepage
+  const homepage = packageJson["homepage"];
+  const adamantEndpoint = homepage.includes("/adamant")
 
-  return (
-    /** Use this for deploying on github-page and change the homepage in package.json to http://*.github.io/adamant */
-    /*
-    <div className="the_app">
-      <Switch>
-        <Redirect exact from="/" to="/adamant" />
-        <Route exact path="/adamant" component={AdamantMain}></Route>
-      </Switch>
-    </div>
-    */
-    /** Use this for deploying with docker or on localhost and change the homepage in package.json to http://*.github.io/ */
-    <div className="the_app">
-      <Switch>
-        <Route exact path="/" component={AdamantMain}></Route>
-      </Switch>
-    </div>
-  );
+  if (adamantEndpoint) {
+    console.log("/adamant endpoint is detected")
+    return (
+      /** Use this for if homepage has /adamant endpoint, this is only for deploying on github-page */
+      <div className="the_app">
+        <Switch>
+          <Redirect exact from="/" to="/adamant" />
+          <Route exact path="/adamant" component={AdamantMain}></Route>
+        </Switch>
+      </div>
+    );
+  } else {
+    return (
+      <div className="the_app">
+        <Switch>
+          <Route exact path="/" component={AdamantMain}></Route>
+        </Switch>
+      </div>
+    );
+  };
 };
